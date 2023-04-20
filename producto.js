3ab2d7bacaa114ef11e17f7f5295faabed1fe718
ToniBaby <mainini.tomi@gmail.com>
2023-04-20 18:32:51 -0300

    Tercera pre entrega

---
+++ producto.js
@@ -0,0 +1,38 @@
+const productos = [{
+    id: 1,
+    img: "imagenes/auricular.jpg",
+    nombre: "Auricular Xpro",
+    precio: 14999
+},
+{
+    id: 2,
+    img: "imagenes/monitor.jpeg",
+    nombre: "Monitor Xpro",
+    precio: 5900
+},
+{
+    id: 3,
+    img: "imagenes/mouse.jpg",
+    nombre: "Mouse Xpro",
+    precio: 26300
+},
+{
+    id: 4,
+    img: "imagenes/pc.jpg",
+    nombre: "PC Gamer",
+    precio: 1200
+},
+{
+    id: 5,
+    img: "imagenes/silla.jpg",
+    nombre: "Silla Gamer",
+    precio: 120000
+},
+{
+    id: 6,
+    img: "imagenes/teclado.jpg",
+    nombre: "Teclado Gamer",
+    precio: 240000
+}
+];
+
